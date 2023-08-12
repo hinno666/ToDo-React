7bd70a5f2e3fd0da7edada37cd5a0048371dfb3a
Andrey <luffytaroyo@gmail.com>
2023-08-12 09:06:34 +0300

    delete inline styles

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -4,48 +4,41 @@ import { MdDone, MdRemoveDone } from "react-icons/md"
 import { BsFillPencilFill } from "react-icons/bs"
 
 
-
 const TodoItem = ({ text, id, deleteTodo, changeTodo }) => {
     const [doneTodo, setDoneTodo] = useState(false);
-    const [editing, setEditing] = useState(false);
+    const [isEdit, setIsEdit] = useState(false);
     const [changeText, setChangeText] = useState(text)
-
-    let editMode = {};
-    if (!editing) {
-        editMode.display = 'none';
-    }
-
-
 
 
     return (
         <div className="todo">
-            <div className="text" style={doneTodo ? { textDecoration: ' line-through', opacity: '0.5' } : { textDecoration: 'none' }}>{editing ? text : changeText}
+            <div className={doneTodo ? 'text text-done' : 'text'}>
+                <p>{isEdit ? text : changeText}</p>
                 <div className="btn-container">
                     <button className="todo-btn done" onClick={() => setDoneTodo(!doneTodo)}>{doneTodo ? <MdRemoveDone /> : <MdDone />}</button>
-                    <button className="todo-btn change" type="button" onClick={() => setEditing(!editing)}><BsFillPencilFill /></button>
+                    <button className="todo-btn change" type="button" onClick={() => setIsEdit(!isEdit)}><BsFillPencilFill /></button>
                     <button className="todo-btn close" type="button" onClick={() => deleteTodo(id)}><RxCross2 /></button>
                 </div>
+                <form className={isEdit ? 'change-input active' : "change-input"} onSubmit={(e) => {
+                    e.preventDefault()
+                    if (changeText.length === 0) {
+                        changeTodo('')
+                    } else {
+                        changeTodo(changeText, id)
+                        setIsEdit(false)
+                    }
+                }}>
+                    <input
+                        type="text"
+                        value={changeText}
+                        className="task-input"
+                        placeholder='Введите текст...'
+                        onChange={(e) => setChangeText(e.target.value)}
+                    />
+                    <button type="submit" className="btn btn-change"> <MdDone /></button>
+                </form>
             </div>
-            <form className="change-input" style={editMode} onSubmit={(e) => {
-                e.preventDefault()
-                if (changeText.length === 0) {
-                    changeTodo('')
-                } else {
-                    changeTodo(changeText, id)
-                    setEditing(false)
 
-                }
-            }}>
-                <input
-                    type="text"
-                    value={changeText}
-                    className="task-input"
-                    placeholder='Введите текст...'
-                    onChange={(e) => setChangeText(e.target.value)}
-                />
-                <button type="submit" className="btn btn-change"> <MdDone /></button>
-            </form>
         </div>
     );
 }
